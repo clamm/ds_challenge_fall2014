2072af1949f98c085a12d99d7b60d33a516795f2
Cindy Lamm <cindy.lamm@comsysto.com>
2015-01-10 20:58:36 +0100

    [smartfly] start off with data analysis

---
+++ problem1_SmartFly/src/exploratory_data_analysis.R
@@ -0,0 +1,8 @@
+# only works if script is sourced
+thisDir <- getwd()
+setwd("~/Projects/DS_challenge/problem1_SmartFly/src")
+
+historicDataFile <- "../data/smartfly_historic.csv"
+scheduledSataFile <- "../data/smartfly_scheduled.csv"
+
+read.table("")
