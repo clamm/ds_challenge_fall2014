a805f47b283c86d35498fe6685cb60799666d62a
Cindy Lamm <cindy.lamm@comsysto.com>
2015-01-15 09:51:01 +0100

    [smartfly] adds files analysis for scheduled data

--- problem1_smartfly/src/00_file_structure_analysis/file_structure_analysis.sh
+++ problem1_smartfly/src/00_file_structure_analysis/file_structure_analysis.sh
@@ -1,32 +1,65 @@
+####### historic data
 
+# get impression of data
+head smartfly_historic.csv
+
+# get number of lines
 wc -l smartfly_historic.csv
 #7374365
 
+# Do all lines have 20 fields?
+awk -F',' '{print NF}' smartfly_historic.csv | uniq
+#-> yes
+
+# Are there single quotes?
+grep -n -o "'" smartfly_historic.csv
+#-> no
+
+# Are there double quotes?
+grep -n -o '"' smartfly_historic.csv
+#-> no
+
+# Which years are contained?
 cat smartfly_historic.csv | awk -F',' '{print $2}' | uniq | sort -n
 #2013
 #2014
-#-> data from these 2 years
 
-awk -F',' '{print NF-1}' smartfly_historic.csv | uniq
-#19
-#-> all lines have 19 commas, i.e. 20 fields
-
-grep -n -o "'" smartfly_historic.csv
-#-> no single quotes
-
-grep -n -o '"' smartfly_historic.csv
-#-> no double quotes
-
+# How many cancelled flights are there?
 awk -F',' '{print $19,$20}' smartfly_historic.csv | grep -v 0 | wc -l
 #104127
-#-> number of canceled flights (1.4% of all flights)
+#-> 1.4% of all flights
 
+# What are values for the cancellation_codes?
 awk -F',' '{print $20}' smartfly_historic.csv | sort | uniq
 # <space>, A, B, C, D, NA
 #-> neccessary conversion of space to NA
 
-# take 3000 random lines of the original historic data as base for prototyping:
-awk 'BEGIN {srand()} {print rand() " " $0}' smartfly_historic.csv | sort | tail -n3000 | sed 's/[^ ]* //' > rand300.csv
-
 # keep every 4000th line of the original historic data as base for prototyping:
 awk 'NR == 1 || NR % 4000 == 0' smartfly_historic.csv > mod4000.csv
+
+
+
+
+####### scheduled data
+
+# get impression of data
+head smartfly_scheduled.csv
+
+# get number of lines
+wc -l smartfly_scheduled.csv
+#566376
+
+# Do all lines have 20 fields?
+awk -F',' '{print NF}' smartfly_historic.csv | uniq
+#->yes
+
+# What about delay, taxi time and cancellation related fieds?
+awk -F',' '{print $13}' smartfly_scheduled.csv | uniq -c
+awk -F',' '{print $17}' smartfly_scheduled.csv | uniq -c
+awk -F',' '{print $18}' smartfly_scheduled.csv | uniq -c
+awk -F',' '{print $19}' smartfly_scheduled.csv | uniq -c
+awk -F',' '{print $20}' smartfly_scheduled.csv | uniq -c
+#-> only contain NA
+
+
+
