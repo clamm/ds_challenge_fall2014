e4375e57ff3195d90084b55bc5b301a2b9f5fedb
Cindy Lamm <cindy.lamm@comsysto.com>
2015-01-19 16:18:41 +0100

    [famous] switches to logistic regression (still only predictions of 1 class)

--- problem2_famous/src/q1b_classify_spam/classify_spam_supervised.py
+++ problem2_famous/src/q1b_classify_spam/classify_spam_supervised.py
@@ -3,7 +3,7 @@ import sys
 
 import numpy as np
 from pyspark import SparkContext
-from pyspark.mllib.classification import NaiveBayes
+from pyspark.mllib.classification import LogisticRegressionWithSGD
 from pyspark.mllib.regression import LabeledPoint
 from writer import Writer
 
@@ -20,29 +20,30 @@ def parseVector(line):
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        usage = "Usage: classify_spam_bayes <train file> <test file> <test id file>"
+        usage = "Usage: classify_supervised <train file> <test file> <test id file>"
         print >> sys.stderr, usage
         exit(-1)
     trainFile = sys.argv[1]
     testFile = sys.argv[2]
     testIdFile = sys.argv[3]
 
-    sc = SparkContext(appName="NaiveBayes")
+    sc = SparkContext(appName="Supervised")
 
     trainLines = sc.textFile(trainFile)
     print("INFO: Read lines of train data file %s" % trainFile)
     trainData = trainLines.map(parseLabeledPoint)
     print("INFO: Start training the model")
-    model = NaiveBayes.train(trainData)
+    model = LogisticRegressionWithSGD.train(trainData)
 
     testLines = sc.textFile(testFile)
     print("INFO: Read lines of test data file %s" % testFile)
     testData = testLines.map(parseVector)
     print("INFO: Predict test data")
+    model.clearThreshold()
     prediction = testData.map(model.predict).collect()
 
     name, extension = os.path.splitext(testFile)
-    outFile = name + "_bayes_prediction" + extension
+    outFile = name + "_supervised_prediction" + extension
     print("INFO: Write prediction result into %s" % outFile)
     writer = Writer(outFile)
     writer.writePrediction(prediction, testIdFile)
--- problem2_famous/src/q1b_classify_spam/submit_bayes.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-spark-submit classify_spam_bayes.py out/supervised/train_supervised.csv out/supervised/test.csv out/supervised/id.csv
