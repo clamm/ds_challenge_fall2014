e36fd54b823dc05a82d47c65c11314eb6645ad21
Cindy Lamm <cindy.lamm@comsysto.com>
2015-01-20 12:55:53 +0100

    [famous] writes answer to q2 in json file

--- problem2_famous/src/q2_compute_ctr/compute_ctr.py
+++ problem2_famous/src/q2_compute_ctr/compute_ctr.py
@@ -4,15 +4,23 @@ import sys
 from pyspark import SparkContext
 
 
+def writeOutputJson(outFile, ctr):
+    result = json.dumps({"overall_ad_click_through_rate": ctr}, indent=True)
+    of = open(outFile, "w+")
+    of.write(result)
+    of.close()
+
+
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        usage = "Usage: compute_ctr <file.json>"
+    if len(sys.argv) != 3:
+        usage = "Usage: compute_ctr <in.json> <out.json>"
         print >> sys.stderr, usage
         exit(-1)
-    jsonFile = sys.argv[1]
+    inJson = sys.argv[1]
+    outJson = sys.argv[2]
 
     sc = SparkContext(appName="CTR")
-    lines = sc.textFile(jsonFile)
+    lines = sc.textFile(inJson)
     data = lines.map(lambda x: json.loads(x))
     nbVisitIds = data.map(lambda x: x["visit_id"]).distinct().count()
     print("INFO: There are %s unique visit_ids" % nbVisitIds)
@@ -20,6 +28,7 @@ if __name__ == "__main__":
     print("INFO: There are in total %s adclicks" % nbAdclicks)
     ctr = float(nbAdclicks) / nbVisitIds
     print("INFO: The overall CTR is %f" % ctr)
+    writeOutputJson(outJson, ctr)
     sc.stop()
 
 
