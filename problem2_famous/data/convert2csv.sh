45ad7659eb054dc2bb55f7ae47f98e6c3ec8f8fb
Cindy Lamm <cindy.lamm@comsysto.com>
2015-01-20 23:54:07 +0100

    [famous] starts off with campaign/query analysis in R

--- problem2_famous/data/convert2csv.sh
+++ problem2_famous/data/convert2csv.sh
@@ -7,8 +7,4 @@ path="$1"
 file=$(basename "$path")
 name="${file%.*}"
 
-< $file json2csv -k visit_id,uid,campaign,tstamp,experiments,action,query | \
-  header -a visit_id,uid,campaign,tstamp,experiments,action,query > $name.csv
-
-
-# json2csv -i=web_visits.log -k=[visit_id,uid,campaign,tstamp,experiments,action,query] -o=web_visits.csv -p=true -v=true
+< $file json2csv -k=visit_id,uid,campaign,tstamp,experiments,action,query -p=true -o=$name.csv
